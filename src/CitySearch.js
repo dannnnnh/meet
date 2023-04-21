21bd642c18b736cf2e70084dfd368e80c4b6c0df
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-21 10:52:22 +0200

    Adding updates

---
+++ src/CitySearch.js
@@ -0,0 +1,49 @@
+// src/CitySearch.js
+
+import React, { Component } from "react";
+
+class CitySearch extends Component {
+  state = {
+    query: "",
+    suggestions: [],
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+    });
+  };
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+          <li>
+            <b>See all cities</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
---
+++ src/mock-data.js
@@ -0,0 +1,16 @@
+// src/mock-data.js
+
+const mockData = [
+    {
+        kind: "calendar#event",
+        etag: '"534534534543"',
+    
+    },
+    {
+        kind: "calendar#event",
+        etag: '"534534534543"',
+    
+    }
+  ];
+  
+  export { mockData };
