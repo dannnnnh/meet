cc4a986db4afdb8424ebaad95324f538a1e92ab5
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-24 20:42:52 +0200

    Upadting address

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
     if (!code) {
       const results = await axios.get(
-        "https://bu80ucml81.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+        "https://am77ht0jik.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
       );
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
