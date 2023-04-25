f3864ba996414e2d4a7eac4679c6c6444793405b
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-25 21:33:26 +0200

    Update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,11 +19,11 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://dannnnnh.github.io/meet/"],
+  redirect_uris: ["https://keen-khapse-6620ca.netlify.app/"],
   javascript_origins: [
     "https://dannnnnh.github.io",
     "http://localhost:3000",
-    "https://dannnnnh.github.io/meet/",
+    "https://keen-khapse-6620ca.netlify.app/",
   ],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
