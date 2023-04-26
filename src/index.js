a42f4bebeaf0975797ebe0443048394c0f62e0f6
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-26 15:30:26 +0200

    Adding Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('5c7dcf6fb47c44109de4fd369dab8b86').install();
 
 //Tst comment
 
@@ -23,3 +26,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+atatus.notify(new Error('Test Atatus Setup'));
