df7b44d193b6a5aef8bc9fb336977065ba88e685
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-27 10:20:43 +0200

    Update

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -7,7 +7,7 @@ class CitySearch extends Component {
     this.state = {
       query: "",
       suggestions: [],
-      showSuggestions: false,
+      showSuggestions: undefined,
       infoText: "",
     };
   }
