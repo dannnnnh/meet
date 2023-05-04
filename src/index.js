0a038b7af43eaaa989739517a5156ed6cd87c8c9
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-05-04 11:47:39 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import * as atatus from "atatus-spa";
 
-//import * as atatus from "atatus-spa";
-
-// atatus.config("5c7dcf6fb47c44109de4fd369dab8b86").install();
-
-//Test comment
+atatus.config("5c7dcf6fb47c44109de4fd369dab8b86").install();
 
 ReactDOM.render(
   <React.StrictMode>
